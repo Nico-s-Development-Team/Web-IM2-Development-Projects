43008c4a65660e6183b5608ffbe4fcb85d688f31
Keanu Ensomo <keanuensomo@Keanus-MacBook-Air.local>
2025-07-08 22:58:13 +0800

    added comment

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ include 'db_conn.php';
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email    = $_POST['email'];
     $password = $_POST['password'];
-
+    // comment
     $stmt = $conn->prepare("SELECT Customer_ID, PasswordHash, is_verified FROM Customer_T WHERE Customer_Email = ?");
     $stmt->bind_param("s", $email);
     $stmt->execute();
